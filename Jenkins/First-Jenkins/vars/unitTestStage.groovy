d443b04d766ea843fc0f59f9ed47b626fcbc23b0
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-27 15:29:20 +0200

    Create unitTestStage.groovy

---
+++ Jenkins/First-Jenkins/vars/unitTestStage.groovy
@@ -0,0 +1,9 @@
+def call() {
+    stage('Unit Test') {
+        steps {
+            echo "Running unit tests"
+            sh "chmod +x gradlew"
+            sh "./gradlew test"
+        }
+    }
+}
