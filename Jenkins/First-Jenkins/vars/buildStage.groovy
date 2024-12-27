145de98ae73a10d29ac91d524e24dc5afdb84e5c
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-27 15:30:43 +0200

    Delete Jenkins/First-Jenkins/vars directory

--- Jenkins/First-Jenkins/vars/buildStage.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-    stage('Build') {
-        steps {
-            echo "Building the project with Gradle"
-            sh "./gradlew build"
-        }
-    }
-}
--- Jenkins/First-Jenkins/vars/unitTestStage.groovy
+++
@@ -1,9 +0,0 @@
-def call() {
-    stage('Unit Test') {
-        steps {
-            echo "Running unit tests"
-            sh "chmod +x gradlew"
-            sh "./gradlew test"
-        }
-    }
-}
