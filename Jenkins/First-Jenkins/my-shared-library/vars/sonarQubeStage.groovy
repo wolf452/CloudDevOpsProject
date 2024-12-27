3c9198ec8d85a1d015b5079a2b12b8ab100c036a
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-27 15:32:39 +0200

    Create sonarQubeStage.groovy

---
+++ Jenkins/First-Jenkins/my-shared-library/vars/sonarQubeStage.groovy
@@ -0,0 +1,10 @@
+def call() {
+    stage('SonarQube Analysis') {
+        steps {
+            withSonarQubeEnv('sonar') {
+                echo "Running SonarQube analysis"
+                sh "./gradlew sonarqube"
+            }
+        }
+    }
+}
