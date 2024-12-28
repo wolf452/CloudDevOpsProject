08dfe26eaa4a9ab3366a5395c17740a12b0bd6f2
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-28 17:16:02 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ivolvegp"
+    bucket         = "test-final-bucket-1"
     key            = "terraform/state.tfstate"
     region         = "us-east-1"
     encrypt        = true
