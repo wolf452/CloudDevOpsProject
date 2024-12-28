c99bb770741cb26c4a42d76cf5bd2b37dac88bfd
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-28 15:50:30 +0200

    Update checkoutStage.groovy

--- Jenkins/First-Jenkins/my-shared-library/vars/checkoutStage.groovy
+++ Jenkins/First-Jenkins/my-shared-library/vars/checkoutStage.groovy
@@ -1,20 +1,19 @@
 def call(Map config = [:]) {
-    pipeline {
-        agent any
-        environment {
-            TERRAFORM_DIR = 'terraform'
-            BACKEND_DIR = 'terraform'
-            ANSIBLE_DIR = 'ansible'
-            INVENTORY_FILE = 'inventory'
-            AWS_ACCESS_KEY_ID = credentials('aws-access-key')  
-            AWS_SECRET_ACCESS_KEY = credentials('aws-secret-key')  
-            AWS_REGION = 'us-east-1'
-        }
-        stages {
-            stage('Checkout') {
-                steps {
-                    git branch: config.gitBranch ?: 'main', url: config.gitUrl ?: 'https://github.com/wolf452/CloudDevOpsProject.git'
-                }
+    // تعديل البيئة والـ Git URL هنا
+    environment {
+        TERRAFORM_DIR = 'terraform'
+        BACKEND_DIR = 'terraform'
+        ANSIBLE_DIR = 'ansible'
+        INVENTORY_FILE = 'inventory'
+        AWS_ACCESS_KEY_ID = credentials('aws-access-key')
+        AWS_SECRET_ACCESS_KEY = credentials('aws-secret-key')
+        AWS_REGION = 'us-east-1'
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: config.gitBranch ?: 'main', url: config.gitUrl ?: 'https://github.com/wolf452/CloudDevOpsProject.git'
             }
         }
     }
