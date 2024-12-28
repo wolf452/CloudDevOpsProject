b35de8465d82082be79c7b8d473889bfe81d1b26
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-28 16:12:32 +0200

    Update checkoutStage.groovy

--- Jenkins/First-Jenkins/my-shared-library/vars/checkoutStage.groovy
+++ Jenkins/First-Jenkins/my-shared-library/vars/checkoutStage.groovy
@@ -1,4 +1,10 @@
 def call(String gitUrl, String branch) {
     echo "Cloning the source code from GitHub"
-    checkout([$class: 'GitSCM', branches: [[name: branch]], userRemoteConfigs: [[url: gitUrl]]])
+    
+    // استخدام checkout مباشرة مع تعريف GitSCM
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: branch]],
+        userRemoteConfigs: [[url: gitUrl]]
+    ])
 }
