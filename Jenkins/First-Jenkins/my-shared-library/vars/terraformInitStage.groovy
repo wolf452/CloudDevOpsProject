5fdd18802e53931959ff6782f013f2aa8f77d35a
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-28 15:23:08 +0200

    Update terraformInitStage.groovy

--- Jenkins/First-Jenkins/my-shared-library/vars/terraformInitStage.groovy
+++ Jenkins/First-Jenkins/my-shared-library/vars/terraformInitStage.groovy
@@ -1,13 +1,17 @@
-def call(Map config = [:]) {
+def terraformInit(Map config = [:]) {
     stage('Terraform Init') {
-        step {
+        steps {
             dir(config.terraformDir ?: 'terraform') {
-                script {
-                    env.TF_VAR_AWS_ACCESS_KEY = env.AWS_ACCESS_KEY_ID
-                    env.TF_VAR_AWS_SECRET_KEY = env.AWS_SECRET_ACCESS_KEY
+                withCredentials([aws(credentialsId: 'aws-cred')]) {
+                    script {
+                        env.TF_VAR_AWS_ACCESS_KEY = env.AWS_ACCESS_KEY_ID
+                        env.TF_VAR_AWS_SECRET_KEY = env.AWS_SECRET_ACCESS_KEY
+                    }
+                    sh 'terraform init -reconfigure'
                 }
-                sh 'terraform init -reconfigure'
             }
         }
     }
+}
+
 }
