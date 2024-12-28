7ec3a91a670344d140c57da888d2ff0d555f6cf3
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-28 16:22:30 +0200

    Update checkoutStage.groovy

--- Jenkins/First-Jenkins/my-shared-library/vars/checkoutStage.groovy
+++ Jenkins/First-Jenkins/my-shared-library/vars/checkoutStage.groovy
@@ -1,13 +1,5 @@
-def checkoutStage(Map config) {
-  checkout([
-    $class: 'GitSCM', 
-    branches: [[name: config.gitBranch]], 
-    userRemoteConfigs: [[url: config.gitUrl]]
-  ])
-}
-
-stage('Checkout') {
-  steps {
-    checkoutStage(gitBranch: 'main', gitUrl: 'https://github.com/wolf452/CloudDevOpsProject.git')
-  }
-}
+checkout([
+  $class: 'GitSCM',
+  branches: [[name: 'main']],
+  userRemoteConfigs: [[url: 'https://github.com/wolf452/CloudDevOpsProject.git']]
+])
