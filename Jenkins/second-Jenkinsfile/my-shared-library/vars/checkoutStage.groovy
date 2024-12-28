5234ec037be73373a0a3e22ef9b00147be567aac
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-28 14:16:38 +0200

    Update checkoutStage.groovy

--- Jenkins/second-Jenkinsfile/my-shared-library/vars/checkoutStage.groovy
+++ Jenkins/second-Jenkinsfile/my-shared-library/vars/checkoutStage.groovy
@@ -1,6 +1,6 @@
 def call(Map params) {
     stage('Checkout') {
-        steps {
+        step {
             echo "Cloning the source code from GitHub"
             git url: params.gitUrl ?: 'https://github.com/wolf452/FinalProjectCode.git', branch: params.gitBranch ?: 'main'
         }
