c8dacf13884a3f9691fbfc396b4639df9c5ce242
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-28 15:30:42 +0200

    Update checkoutStage.groovy

--- Jenkins/First-Jenkins/my-shared-library/vars/checkoutStage.groovy
+++ Jenkins/First-Jenkins/my-shared-library/vars/checkoutStage.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]) {
     stage('Checkout') {
-        step {
+        steps {
             git branch: config.gitBranch ?: 'main', url: config.gitUrl ?: 'https://github.com/wolf452/CloudDevOpsProject.git'
         }
     }
