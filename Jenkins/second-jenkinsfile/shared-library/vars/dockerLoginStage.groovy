1f297ccbf08690649e962bc385a477c84ded0afb
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-29 03:38:30 +0200

    Update dockerLoginStage.groovy

--- Jenkins/second-jenkinsfile/shared-library/vars/dockerLoginStage.groovy
+++ Jenkins/second-jenkinsfile/shared-library/vars/dockerLoginStage.groovy
@@ -1,8 +1,11 @@
-// dockerLoginStage.groovy
-def call() {
-    script {
-        withCredentials([usernamePassword(credentialsId: 'Docker_hub', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-            sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
+f call() {
+    stage('Login to Docker Hub') {
+        steps {
+            script {
+                withCredentials([usernamePassword(credentialsId: 'Docker_hub', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                    sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
+                }
+            }
         }
     }
 }
