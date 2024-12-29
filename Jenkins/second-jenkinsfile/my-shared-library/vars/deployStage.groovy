35e9bb4ccf9db84872df0c33511411280ae842c2
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-29 20:55:40 +0200

    Update deployStage.groovy

--- Jenkins/second-jenkinsfile/my-shared-library/vars/deployStage.groovy
+++ Jenkins/second-jenkinsfile/my-shared-library/vars/deployStage.groovy
@@ -1,5 +1,5 @@
 // vars/deployStage.groovy
-def call(kubeconfigPath, ./deploymentYaml) {
+def call(kubeconfigPath, deploymentYaml) {
     echo "Deploying the application to Kubernetes"
     sh "kubectl --kubeconfig=${kubeconfigPath} apply -f ${deploymentYaml}"
 }
