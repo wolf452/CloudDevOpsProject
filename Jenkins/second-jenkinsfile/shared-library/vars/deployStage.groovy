877e7d94ab92d1ee6314e2eca7f6f32a6ddd8364
ahmedmaher <121304760+wolf452@users.noreply.github.com>
2024-12-29 03:43:28 +0200

    Delete Jenkins/second-jenkinsfile/shared-library/vars directory

--- Jenkins/second-jenkinsfile/shared-library/vars/deployStage.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-    stage('Deploy to Kubernetes') {
-        steps {
-            echo "Deploying the application to Kubernetes"
-            sh "kubectl --kubeconfig=/home/ubuntu/jenkins/.kube/config apply -f $DEPLOYMENT_YAML"
-        }
-    }
-}
--- Jenkins/second-jenkinsfile/shared-library/vars/dockerBuildStage.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-    stage('Build Docker Image') {
-        steps {
-            echo "Building Docker image: $DOCKER_IMAGE"
-            sh "docker build -t $DOCKER_IMAGE ."
-        }
-    }
-}
